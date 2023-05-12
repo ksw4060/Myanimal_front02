6b49c635e8240382f714407d2ac9f8fc46c56076
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-13 03:20:29 +0900

    게시글 삭제하기 구현 완료

--- assets/js/article_detail.js
+++ assets/js/article_detail.js
@@ -6,24 +6,23 @@ window.onload = () => {
 }
 
 const article_id = new URLSearchParams(window.location.search).get('id');
-console.log(article_id)
-async function ArticleDetail(article_id){
-    
-    
+async function ArticleDetail(article_id) {
+
+
     const response = await fetch(`${backend_base_url}/articles/${article_id}`, {
-        method: 'GET',        
+        method: 'GET',
     })
 
-    response_json = await response.json()    
+    response_json = await response.json()
     console.log(response_json)
-    
+
     const category = document.querySelector('#category');
     const author = document.querySelector('#author');
     const article_title = document.querySelector('#article-title');
     const article_created_at = document.querySelector('#article-created-at');
     const article_updated_at = document.querySelector('#article-updated-at');
     const article_content = document.querySelector('#article-content');
-    
+
     // const detail_product_img_url = `${BACKEND_API}/${response_json.image}`
 
     // detail_product_img.setAttribute('src', detail_product_img_url)
@@ -33,5 +32,26 @@ async function ArticleDetail(article_id){
     article_created_at.innerText = response_json.article_created_at
     article_updated_at.innerText = response_json.article_updated_at
     article_content.innerText = response_json.article_content
-    
+
+}
+
+
+// 글 삭제
+
+async function ArticleDelete() {
+
+    console.log(article_id)
+    const response = await fetch(`${backend_base_url}/articles/${article_id}`, {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("access"),
+            'content-type': 'application/json',
+        },
+        method: 'DELETE',
+    })
+    if (response.status === 204) {
+        alert("삭제 완료!")
+        location.replace('index.html')
+    }else{
+        alert("권한이 없습니다.")
+    }
 }
