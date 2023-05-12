5957cdce27f7d55eda99f304fb9427e978c5db8e
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-12 17:09:01 +0900

    글 수정

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,3 +1,8 @@
+const frontend_base_url = "http://127.0.0.1:5000"
+const backend_base_url = "http://127.0.0.1:8000"
+
+
+// 글작성
 async function save_article() {
     const title = document.getElementById("title").value
     const content = document.getElementById("content").value
@@ -5,7 +10,7 @@ async function save_article() {
     const img = document.getElementById("img").value
     const token = localStorage.getItem("access")
 
-    const response = await fetch('http://127.0.0.1:8000/articles/', {
+    const response = await fetch(`${backend_base_url}/articles/`, {
         headers: {
             "content-type":"application/json",
             "Authorization": "Bearer " + localStorage.getItem("access"),
@@ -18,7 +23,38 @@ async function save_article() {
             "article_img": img,
         })
     })
-    console.log()
+
+    if (response.status == 200) {
+        alert("글 작성 완료")
+        window.location.replace('index.html')
+    } else if (title == '' || content == '' || category == '' ) {
+        alert("빈칸을 입력해 주세요.")
+    }
+
 }
 
 
+// 글 수정
+async function UpdateArticle() {
+
+    const response = await fetch(`${backend_base_url}/article/${article_id}`, {
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("access"),
+            'content-type': 'application/json',
+        },
+        method: 'GET',
+        body: JSON.stringify({
+            "article_title": title,
+            "article_content": content,
+            "category": category,
+            "article_img": img
+        })
+    })
+
+    if (response.status == 200) {
+        alert("글 작성 완료")
+        window.location.replace('index.html')
+    } else if (title == '' || content == '' || category == '' ) {
+        alert("빈칸을 입력해 주세요.")
+    }
+}
