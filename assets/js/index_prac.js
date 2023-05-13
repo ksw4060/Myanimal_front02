776d1fc5f0f6ddacbec700af3cfb69d42b61d0fc
sojin park <psjin977@naver.com>
2023-05-14 06:56:04 +0900

    pagination 수정

--- assets/js/index_prac.js
+++ assets/js/index_prac.js
@@ -1,72 +1,50 @@
+console.log("찍힘")
 
-// const page = document.querySelector('#page');; //ul
-// let pageNumber = document.getElementById('page_number'); //페이지넘버를 담는 변수
-// let current_page = 1; // 현재 시작 페이지 넘버는 1로 지정한다.
-// let records_per_page = 8 // 한 페이지당 보여질 게시글 수
-// const rows = document.querySelectorAll('#my-table ul li');
-// const rowsCount = rows.length; // 게시글의 갯수 카운트
-
-// let pageNumbers = function () {
-//   let pageNumber = document.getElementById('page_number');
-//   pageNumber.innerHTML = "";
-
-//   for (let i = 1; i < numPages() + 1; i++) {
-//     // 페이지 수 만큼 innerHTML를 이용해 숫자를 넣어준다.
-//     pageNumber.innerHTML += `<span class="page-item" aria-current="page"><a class="page-link" href="">${i}</a></span>`;
-//   }
-
-// }
-// let numPages = function () {
-//   //게시글 수를 보여질 게시글 수로 나눈 뒤 올림연산하여 페이지 수를 구한다.
-//   return Math.ceil(rowsCount / records_per_page);
-// }
-
-// let changePage = function (page) {
-//   const ul = document.querySelector('ul.discussions__container');
-
-//   if (page < 1) {
-//     page = 1;
-//   }
-//   if (page > (numPages() - 1)) {
-//     page = numPages();
-//   }
-
-//   ul.innerHTML = "";
-
-//   for (var i = (page - 1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
-//     ul.innerHTML += "<div class='objectBlock'>" + Json[i].id + "</div>";
-//   }
-//json파일의 id값이 10번출력됨.
-// }
+async function nextPage() {
+    const response = await fetch(`${backend_base_url}/articles/pagination/?page=2`);
+    const data = await response.json();
+    // 페이지네이션한 게시글들은 object객체로 받아옴 해당 객체를 배열로 만들고 null값은 건너뛰어서 오류발생 활률을 낮춤
+    const articles = Array.isArray(data) ? data : Object.values(data).filter(article => article != null);
+    console.log(articles)
 
 
 
-let rowPerPage = 8; //한 페이지당 표시되는 게시글의 수
-const rows = document.querySelectorAll('#my-table ul li');
-const rowsCount = rows.length; // 게시글의 갯수 카운트
-const pageCount = Math.ceil(rowsCount / rowPerPage); //총 갯수/모든게시물을 나눳을때 무조건 올림으로 페이지를 만들어줌
-const page = document.querySelector('#page'); //id 지정해준 ul을 쿼리셋으로 저장
-let current_page = 1; //현재페이지는 1
+    const articleList = document.getElementById('article-list');
+    articleList.innerHTML = '';
+    // data의 results에 json 형식으로 article 데이터s를 가져옴 그냥 article.id로 하니까 안가져와져서 여차저차 찾아냄
+    data.results.forEach((article) => {
 
-async function pagination() {
+        const articleCard = document.createElement('div');
+        articleCard.classList.add('col', 'mb-5');
+        const title = article.article_title;
+        const image = article.article_image;
+        const category = article.category;
+        // 아래 html이 반복되어 입력됨
+        // 이미지의 경우 이미지 경로에 이미지가 있으면 보여주고 없을경우(?) 기본이미지를 가져옴..저희집 고양이 귀엽죠 여러분
+        // 채연 : 넹 너무 기여워요 ㅠ 어케 이케 이쁘지 저 납치 해도 될까요? 물론 소진님을
+        articleCard.innerHTML = `
+      <a style="text-decoration:none; color:black;" href="article_detail.html?id=${article.id}">
+      <div class="card h-100">
+        <img class="card-img-top" src="${image ? image : './assets/images/IMG_8257.jpg'}" alt="" />
+        <div class="card-body p-4">
+          <div class="text-center">
+            <h5 class="fw-bolder">${title}</h5>
+            <label for="" class="category">${category}</label>
+            <hr>
+            <label for="" class="user">${payload_parse.account}</label>
+              </div>
+            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              
+            </div>
+          </div>
+          </a>
+        `;
 
-    //페이지네이션 생성
-    /* <li class="page-item active" aria-current="page"><a class="page-link" href="#!">1</a></li> 
-    for (초기값;조건;증감){}
-    */
+        articleList.appendChild(articleCard);
 
-    for (let i = 1; i <= pageCount; i++) {
-        page.innerHTML = `<li class="page-item active" aria-current="page"><a class="page-link" href="">${i}</a></li>`;
-        page.innerHTML += `<li class="page-item active" aria-current="page"><a class="page-link" href="">${i + 1}</a></li>`;
-    }
+    });
 
-    console.log(rows);
-    function displayRow(idx) {
-        const rowsArray = [...rows];
-        console.log(rowsArray)
-    }
 
 }
-
-pagination();
 
