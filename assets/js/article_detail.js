c02d82e8b91f7bcb50ec21a82665847ae80ece20
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-14 06:36:16 +0900

    댓글 수정 완료

--- assets/js/article_detail.js
+++ assets/js/article_detail.js
@@ -100,17 +100,17 @@ async function loadComments(article_id) {
     const comments = await response.json();
     console.log(comments)
 
-    
+
 
 
     comments.forEach((comment) => {
         const commentList = document.getElementById('comment-list');
-        commentList.insertAdjacentHTML ('beforeend',`
+        commentList.insertAdjacentHTML('beforeend', `
         
-        <div class="card-header">
+        <div  class="card-header">
                 <a>${comment.user}</a>
             </div>
-            <div class="card-body" style="max-width: 1000px;">
+            <div id="comment-${comment.id}" class="card-body" style="max-width: 1000px;">
                 <div class="row g-5">
                     <!-- 유저 프로필 사진 -->
                     <div class="col-md-4" style="width: 200px;">
@@ -120,11 +120,11 @@ async function loadComments(article_id) {
                     <!-- 댓글 제목과 내용 입력-->
                     <div class="col-md-8">
                         <div class="card-body">
-                            <p class="card-text">${comment.comment}</p>
+                            <p class="card-text" id="comment-content-${comment.id}">${comment.comment}</p>
 
                         </div>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <a href="#" class="btn btn-secondary btn-sm me-md-2">댓글수정</a>
+                            <a href="#" onclick = "editComment(${comment.id})" class="btn btn-secondary btn-sm me-md-2">댓글수정</a>
                             <a href="#" onclick = "CommentDelete(${comment.id})" class="btn btn-secondary btn-sm">댓글삭제</a>
                         </div>
                         <p class="card-text"><small class="text-muted">${comment.comment_created_at}</small></p>
@@ -136,6 +136,61 @@ async function loadComments(article_id) {
         `);
     });
 }
+
+
+// 댓글 수정 폼 열기
+async function editComment(comment_id) {
+    event.preventDefault();
+
+    const commentCard = document.getElementById(`comment-${comment_id}`);
+    const before_comment = document.getElementById(`comment-content-${comment_id}`);
+
+    const editForm = document.createElement('div');
+    editForm.classList.add('edit-form');
+
+    const textarea = document.createElement('textarea');
+    textarea.classList.add('form-control');
+    console.log(before_comment.innerText)
+    textarea.value = before_comment.innerText;
+
+    const saveButton = document.createElement('button');
+    saveButton.classList.add('btn', 'btn-primary', 'btn-sm');
+    saveButton.textContent = '수정 완료';
+    saveButton.addEventListener('click', () => {
+        updateComment(comment_id, textarea.value);
+    });
+
+    editForm.appendChild(textarea);
+    editForm.appendChild(saveButton);
+
+    commentCard.querySelector('.card-body').appendChild(editForm);
+    before_comment.style.display = 'none'; // 기존 댓글 숨기기
+}
+// 댓글 수정 저장하기
+async function updateComment(commentId, newContent) {
+    const token = localStorage.getItem('access');
+    const response = await fetch(`${backend_base_url}/articles/${article_id}/comment/${commentId}/`, {
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        method: 'PUT',
+        body: JSON.stringify({
+            'comment': newContent,
+        }),
+    });
+
+    if (response.status === 200) {
+        alert('댓글이 수정되었습니다.');
+        location.reload();
+    } else if ((response.status === 403)) {
+        alert(response.message);
+    } else {
+        alert('댓글 수정에 실패했습니다.');
+    }
+}
+
+
 
 // 댓글 삭제
 async function CommentDelete(comment_id) {
@@ -157,16 +212,16 @@ async function CommentDelete(comment_id) {
 
 
 // 좋아요 누르기
-async function ClickHeart(){
+async function ClickHeart() {
 
-    const response = await fetch(`${backend_base_url}/articles/${article_id}/hearts/`,{
-        headers:{
+    const response = await fetch(`${backend_base_url}/articles/${article_id}/hearts/`, {
+        headers: {
             "Authorization": "Bearer " + localStorage.getItem("access"),
             'content-type': 'application/json',
         },
-        method : 'POST',
+        method: 'POST',
     })
-    if(response.status === 200){
+    if (response.status === 200) {
         alert("❤️")
         location.reload();
     }
@@ -174,14 +229,14 @@ async function ClickHeart(){
 
 
 // 좋아요 갯수
-async function CountHeart(){
+async function CountHeart() {
 
-    const response = await fetch(`${backend_base_url}/articles/${article_id}/hearts/`,{
-        headers:{
-            
+    const response = await fetch(`${backend_base_url}/articles/${article_id}/hearts/`, {
+        headers: {
+
             'content-type': 'application/json',
         },
-        method : 'GET',
+        method: 'GET',
     })
     response_json = await response.json()
     document.getElementById('heart-count').innerText = response_json.hearts
@@ -189,16 +244,16 @@ async function CountHeart(){
 
 
 // 북마크 누르기
-async function ClickBookmark(){
+async function ClickBookmark() {
 
-    const response = await fetch(`${backend_base_url}/articles/${article_id}/bookmarks/`,{
-        headers:{
+    const response = await fetch(`${backend_base_url}/articles/${article_id}/bookmarks/`, {
+        headers: {
             "Authorization": "Bearer " + localStorage.getItem("access"),
             'content-type': 'application/json',
         },
-        method : 'POST',
+        method: 'POST',
     })
-    if(response.status === 200){
+    if (response.status === 200) {
         alert("북마크")
         location.reload();
     }
@@ -206,14 +261,14 @@ async function ClickBookmark(){
 
 
 // 북마크 갯수
-async function CountBookmark(){
+async function CountBookmark() {
 
-    const response = await fetch(`${backend_base_url}/articles/${article_id}/bookmarks/`,{
-        headers:{
-            
+    const response = await fetch(`${backend_base_url}/articles/${article_id}/bookmarks/`, {
+        headers: {
+
             'content-type': 'application/json',
         },
-        method : 'GET',
+        method: 'GET',
     })
     response_json = await response.json()
     console.log(response_json.bookmarks)
