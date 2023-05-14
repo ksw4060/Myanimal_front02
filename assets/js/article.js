52e6a652cb7b14d9b4715f02f14890a817e8be65
Chaeyeon Seo <ssallyseo@gmail.com>
2023-05-14 17:41:36 +0900

    게시글 이미지 업로드 미리보기 구현

--- assets/js/article.js
+++ assets/js/article.js
@@ -41,3 +41,21 @@ async function save_article() {
 }
 
 
+// 이미지 업로드 미리보기
+function setThumbnail(event) {
+    let reader = new FileReader();
+
+    reader.onload = function (event) {
+      let img = document.createElement("img");
+      img.setAttribute("src", event.target.result);
+
+      // 썸네일 크기 조절
+      img.style.width = "500px"; // 너비 500px로 설정
+      img.style.height = "auto"; // 높이 자동 설정
+      document
+        .querySelector("#imgthumbnail")
+        .appendChild(img);
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  }
